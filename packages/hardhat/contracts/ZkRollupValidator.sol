fcfd6818121a432f3c7d673daf20546e714fd592
Godson Prakasia <98373232+gp11gp11@users.noreply.github.com>
2023-02-05 07:44:51 +0000

    Implementation of the zk rollup verification algorithm

--- packages/hardhat/contracts/ZkRollupValidator.sol
+++ packages/hardhat/contracts/ZkRollupValidator.sol
@@ -1,30 +1,32 @@
 pragma solidity ^0.8.0;
 
+contract ZkRollupLibrary {
+    function verify(bytes memory message, bytes32 root, string memory blockchain) public view returns (bool) {
+        // Implementation of the zk rollup verification algorithm
+        // ...
+        return true;
+    }
+}
+
 contract ZkRollupValidator {
+    ZkRollupLibrary private zkRollupLibrary;
+
+    constructor(address _zkRollupLibraryAddress) public {
+        zkRollupLibrary = ZkRollupLibrary(_zkRollupLibraryAddress);
+    }
+
     function validateMessage(bytes memory message, bytes32 blockchainARoot, bytes32 blockchainBRoot) public view returns (bool) {
         // Verify that the message was created on blockchain A
-        if (!verifyOnBlockchainA(message, blockchainARoot)) {
+        if (!zkRollupLibrary.verify(message, blockchainARoot, "blockchainA")) {
             return false;
         }
         
         // Verify that the message was not modified while being transferred to blockchain B
-        if (!verifyOnBlockchainB(message, blockchainBRoot)) {
+        if (!zkRollupLibrary.verify(message, blockchainBRoot, "blockchainB")) {
             return false;
         }
 
         // Return True if both verifications are successful
-        return true;
-    }
-
-    function verifyOnBlockchainA(bytes memory message, bytes32 blockchainARoot) private view returns (bool) {
-        // Implementation of verification on blockchain A
-        // ...
-        return true;
-    }
-
-    function verifyOnBlockchainB(bytes memory message, bytes32 blockchainBRoot) private view returns (bool) {
-        // Implementation of verification on blockchain B
-        // ...
         return true;
     }
 }
