7b05f62fde70d68e2c30eda631cd46aca25c2aa3
Abhinav <abhinavtb@gmail.com>
2023-02-06 13:14:54 +0530

    server setup

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,9 +1,12 @@
-const ethers = require('ethers');
-const CONTRACT_ADDRESS = "0x10820dB......";
-const ABIJSON = require('./abi.json');
-provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
-const contract = new ethers.Contract(CONTRACT_ADDRESS, ABIJSON.abi, provider);
+import abi from "./abi.js";
+import { ethers } from "ethers";
+const CONTRACT_ADDRESS = "0xbAE08f18a78ada7b4dDceEbb7869B23C414Dd1c4";
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://eth-goerli.g.alchemy.com/v2/HP6JXNqDqlgBJTtlGYVh2iugTgcp4OpT%27"
+);
+const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
 
-contract.on("eventName", ( caller,tokenID) => {
-      //this section is called every time an event is emitted 
-})
+contract.on("DispatchMessage", (caller, tokenID) => {
+  //this section is called every time an event is emitted
+  console.log(caller);
+});
